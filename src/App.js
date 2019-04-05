a7bd6d85bc79ecd67be53151ae78c24abb0bc53d
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-04-05 22:52:20 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import panda from './panda.jpg';
 import logo from './web_logo_VS.png';
 import './App.css';
 
@@ -11,11 +10,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
-            href="https://www.latlmes.com/arts/return-of-the-golden-age-of-comics-1"
+            href="https://www.github.com/vaithschmitz"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Click Me
+            Github
+          </a>
+          <a
+            className="App-link"
+            href="https://www.twitter.com/realvaith"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
           </a>
         </header>
       </div>
