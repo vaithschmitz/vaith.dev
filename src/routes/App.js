816f0a2c2b8e43719a10a0bc8720d3dfc14018ab
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-04-06 23:23:12 +0100

    broken contact page

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import logo from './web_logo_VS.png';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Contact from './Contact';
+import logo from '../web_logo_VS.png';
+import Home from './Home'
 import './App.css';
 
 class App extends Component {
@@ -7,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
             href="https://www.github.com/vaithschmitz"
@@ -23,7 +26,14 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Twitter
-          </a>
+          </a> */}
+          <Router>
+              <Link to="/contact">Contact</Link>
+            <Switch>
+              <Route path="/" component={Home} exact/>
+              <Route path="/contact" component={Contact} exact/>
+            </Switch>
+          </Router>
         </header>
       </div>
     );
---
+++ src/routes/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+    return (
+        <div className='maintext'>
+            Please Don't Contact Me, I like To Be Alone.   
+        </div>
+    )
+}
+
+export default Contact
