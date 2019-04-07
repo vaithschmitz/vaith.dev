6c15b321c6a66a851608654e60cd2f2dd8b3f6eb
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-04-07 09:55:48 +0100

    extract routing logic to index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Switch, NavLink } from "react-router-dom";
 import './index.css';
+import Contact from './routes/Contact';
 import App from './routes/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const routing = ( 
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <NavLink exact activeClassName="active" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/contact">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/contact" component={Contact} exact />
+        </Switch>
+      </div>
+    </Router>
+  )
+
+
+
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import Contact from './Contact';
 import logo from '../web_logo_VS.png';
-import Home from './Home'
 import './App.css';
 
 class App extends Component {
@@ -10,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
             href="https://www.github.com/vaithschmitz"
@@ -26,14 +23,7 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Twitter
-          </a> */}
-          <Router>
-              <Link to="/contact">Contact</Link>
-            <Switch>
-              <Route path="/" component={Home} exact/>
-              <Route path="/contact" component={Contact} exact/>
-            </Switch>
-          </Router>
+          </a>
         </header>
       </div>
     );
