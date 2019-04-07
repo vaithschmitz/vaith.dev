5c969a212257a9c6d91a9e2be4b3cf3257b7a84f
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-04-07 11:05:28 +0100

    added css grid on landing links

--- src/routes/App.js
+++ src/routes/App.js
@@ -10,13 +10,26 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-buttons">
+          <Link to="/projects">
+            <Button 
+              id="btn1"
+              variant="contained" color="primary">Projects
+            </Button> 
+          </Link>
+          <Link to="/contact">
+            <Button
+              id="btn2" variant="contained" color="primary" >Contact
+            </Button> 
+          </Link>
           <a
             className="App-link"
             href="https://www.github.com/vaithschmitz"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Button variant="contained" color="primary">Github
+            <Button
+              id="btn3" variant="contained" color="primary" fullWidth="true">Github
             </Button>        
           </a>
           <a
@@ -25,17 +38,11 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Button variant="contained" color="primary">Twitter
+            <Button 
+              id="btn4" variant="contained" color="primary" fullWidth="true">Twitter
             </Button> 
           </a>
-          <Link to="/projects">
-            <Button variant="contained" color="primary">Projects
-            </Button> 
-          </Link>
-          <Link to="/contact">
-            <Button variant="contained" color="primary">Contact
-            </Button> 
-          </Link>
+          </div>
         </header>
       </div>
     );
