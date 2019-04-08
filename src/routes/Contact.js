53ecde5b977fa2fc13514bf19e15940ea6518711
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-04-08 16:51:37 +0100

    added links to li/al

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Fab from '@material-ui/core/Fab';
+import Button from '@material-ui/core/Button';
 import './Contact.css'
 
 
 const Contact = () => {
     return (            
         <div className='maintext'>
-            Under Construction.   
-            <Link to="/">      
-                <Fab variant="extended" color="primary">Home
-                </Fab> 
-            </Link>
+            <div className='button'>
+                <Button variant="contained" color="primary" href="https://angel.co/vaith-schmitz">AngelList</Button>
+            </div>
+            <div className='button'>
+                <Button variant="contained" color="primary" href="https://linkedin.com/in/vaithschmitz">LinkedIn</Button>
+            </div>
+            <div className='button'>
+                <Link to="/">      
+                    <Button variant="contained" color="secondary">Home
+                    </Button> 
+                </Link>
+            </div>
         </div>
     )
 }
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Fab from '@material-ui/core/Fab';
+import Button from '@material-ui/core/Button';
 import './Projects.css'
 
 
@@ -9,8 +9,8 @@ const Projects = () => {
         <div className='maintext'>
             Under Construction.   
             <Link to="/">      
-                <Fab variant="extended" color="primary">Home
-                </Fab> 
+                <Button variant="contained" color="secondary">Home
+                </Button> 
             </Link>
         </div>
 
