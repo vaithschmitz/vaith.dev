c715d11386aa8847863d2f7121ddbfa5f8c99684
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-08-04 14:53:40 +0100

    init grid

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,40 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import Button from '@material-ui/core/Button';
 import logo from '../web_logo_VS.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-buttons">
-{/* 
-            <Link to="/projects">
-              <Button 
-                id="btn1"
-                variant="outlined" color="primary" >Projects
-              </Button> 
-            </Link>
-       */}
-            <Button
-              id="btn3" variant="outlined" color="primary"  href="https://www.github.com/vaithschmitz">Github
-            </Button>        
+      <div id="App">
+        <div id='App-logo-container'>
+          <img src={logo} id='App-logo' alt="logo" />
+        </div>
+          
 
-            <Button 
-              id="btn4" variant="outlined" color="primary"  href="https://www.twitter.com/realvaith">Twitter
-            </Button> 
-            
-            <Link to="/contact">
-              <Button
-                id="btn2" variant="outlined"  color="secondary" >Contact
-              </Button> 
-            </Link>
-
-          </div>
-        </header>
       </div>
     );
   }
