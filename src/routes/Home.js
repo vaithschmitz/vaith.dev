dfc4fb472272f466ea0b67d1cea69c110e119cbc
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-08-05 19:51:38 +0100

    added boring words

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,12 +8,14 @@ import './Home.css';
 export default function Home () {
   
     
-    const [boringWord, setBoringWord] = useState('cunt')
-    const boringWordList = ['dull','humdrum','lifeless','monotonous','mundane','stale','stodgy','stuffy','stupid','tame','tedious','tiresome','tiring','trite','uninteresting', 'boring']
-    // const boringWord = () => {
-    //   let x = Math.floor(Math.random()* boringWordList.length)
-    //   return boringWordList[x]
-    // }
+    const [boringWord, setBoringWord] = useState('lame')
+    const boringWordList = ['dull','humdrum','lifeless','monotonous','mundane','stale','stodgy','stuffy','stupid','tame','tedious','tiresome','tiring','trite','uninteresting', 'boring', 'lame', 'meh', 'uhm', 'buzzkill']
+
+    // setBoringWord(boringWordList[Math.floor(Math.random()* boringWordList.length)]
+    const switchWord =()=>{
+      setBoringWord(boringWordList[Math.floor(Math.random()* boringWordList.length)])
+    }
+    setTimeout(switchWord, 1000)
 
     return (
       <div id="Home">
@@ -22,6 +24,7 @@ export default function Home () {
         </div>
         <div id='homeText'>
           <p>Literally the most {boringWord} page, click on something up there.</p>
+          {/* <div onClick={()=> setBoringWord(boringWordList[Math.floor(Math.random()* boringWordList.length)])}>CLICK</div> */}
         </div>
 
 
