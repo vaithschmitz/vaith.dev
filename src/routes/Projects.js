b7b7eb02f3c0d399544d3a272f522d13388a0295
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-08-05 18:37:40 +0100

    contact form

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,14 +1,14 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import Button from '@material-ui/core/Button';
-// import Card from '@material-ui/core/Card';
-// import CardHeader from '@material-ui/core/CardHeader';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardActions from '@material-ui/core/CardActions';
-// import Typography from '@material-ui/core/Typography'
-// import logo from '../web_logo_VS.png';
-// import './Projects.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography'
+import logo from '../web_logo_VS.png';
+import './Projects.css'
 
 // const MyCardMedia = (
 //     <CardMedia  
@@ -16,81 +16,11 @@
 //     image={logo}/>
 // ) 
 
-// const Projects = () => {
-//     return(
-//         <div className='maintext'>
-//             <div className="cards">
+const Projects = () => {
+    return(
+        <div className='maintext'>
+        </div>
+    )
+}
 
-//                 <div className="single-card">
-//                     <Card >
-//                         <CardHeader title="Project 1"/>
-//                         {MyCardMedia}
-//                         <CardContent>
-//                             <Typography component="p">
-//                                 This is a project with things and stuff.
-//                             </Typography>
-//                         </CardContent>
-//                     </Card>
-//                 </div>
-
-//                 <div className="single-card">
-//                     <Card>
-//                         <CardHeader title="Project 2"/>
-//                         {MyCardMedia}
-//                         <CardContent>
-//                             <Typography component="p">
-//                                 This is another project with things and stuff.
-//                             </Typography>
-//                         </CardContent>
-//                     </Card>
-//                 </div>
-
-//                 <div className="single-card">
-//                     <Card >
-//                         <CardHeader title="Project 3"/>
-//                         {MyCardMedia}
-//                         <CardContent>
-//                             <Typography component="p">
-//                                 And here's yet another one.
-//                             </Typography>
-//                         </CardContent>
-//                     </Card>
-//                 </div>   
-
-//                 <div className="single-card">
-//                     <Card >
-//                         <CardHeader title="Project 4"/>
-//                         {MyCardMedia}
-//                         <CardContent>
-//                             <Typography component="p">
-//                                 Hello, did anybody say cheesecake. Or beef cake? Or pie?
-//                             </Typography>
-//                         </CardContent>
-//                     </Card>
-//                 </div>
-
-//                 <div className="single-card">
-//                     <Card >
-//                         <CardHeader title="Project 5"/>
-//                         {MyCardMedia}
-//                         <CardContent>
-//                             <Typography component="p">
-//                             This is a project with things and stuff.
-//                             </Typography>
-//                         </CardContent>
-//                     </Card>
-//                 </div>
-
-//             </div>
-
-//             <Link to="/">      
-//                 <Button variant="outlined" color="secondary">Home
-//                 </Button> 
-//             </Link>
-//         </div>
-        
-
-//     )
-// }
-
-// export default Projects;
+export default Projects;
