f57ae67457624d1a812ce1425d1ad333b6f6d26d
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-08-05 19:30:51 +0100

    switching home route to hooks

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Link } from "react-router-dom";
 import Navbar from '../components/Navbar'
 import logo from '../web_logo_VS.png';
 import './Home.css';
 
 
-class Home extends Component {
-  render() {
+export default function Home () {
+  
+    
+    const [boringWord, setBoringWord] = useState('cunt')
+    const boringWordList = ['dull','humdrum','lifeless','monotonous','mundane','stale','stodgy','stuffy','stupid','tame','tedious','tiresome','tiring','trite','uninteresting', 'boring']
+    // const boringWord = () => {
+    //   let x = Math.floor(Math.random()* boringWordList.length)
+    //   return boringWordList[x]
+    // }
+
     return (
       <div id="Home">
         <div id='Home-logo-container'>
           <img src={logo} id='Home-logo' alt="logo" />
         </div>
         <div id='homeText'>
-          <p>Literally the most boring page, click on something up there.</p>
+          <p>Literally the most {boringWord} page, click on something up there.</p>
         </div>
 
 
       </div>
     );
-  }
+
 }
 
-export default Home;
+
+
