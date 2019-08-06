f542f2a8fc1b850f695cf798949280305477d0c1
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-08-06 20:43:14 +0100

    viewport units on contact form labels

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,12 @@ import twitter from './Twitter.png'
 import './Footer.css'
 
 export default function Footer(){
-    
 
     return(
         <div id='FooterContainer'>
             <a id='Footer-linkedin' href='https://www.linkedin.com/in/vaithschmitz/' alt='link to linkedin'><img id='Footer-linkedin-image' src={linkedin}></img></a>
-            <a id='Footer-github' href='https://github.com/vaithschmitz' alt='link to github'>><img id='Footer-github-image' src={github}></img></a>
-            <a id='Footer-twitter' href='https://twitter.com/realvaith' alt='link to twitter'>><img id='Footer-twitter-image' src={twitter}></img></a>
+            <a id='Footer-github' href='https://github.com/vaithschmitz' alt='link to github'><img id='Footer-github-image' src={github}></img></a>
+            <a id='Footer-twitter' href='https://twitter.com/realvaith' alt='link to twitter'><img id='Footer-twitter-image' src={twitter}></img></a>
         </div>
     )
 }
