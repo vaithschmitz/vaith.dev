91ae224da7202c6b82e5642755127a755b338973
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-08-06 18:39:26 +0100

    .

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from 'react';
 import { Link } from "react-router-dom";
-import Navbar from '../components/Navbar'
+
 import logo from '../web_logo_VS.png';
 import './Home.css';
 
